b629fd4886b53f92090179b8d2553afcad97dfed
khanahmedm <81051236+khanahmedm@users.noreply.github.com>
2021-06-27 23:31:49 -0500

    Create backupFiles.sh

---
+++ LinuxShellScripting/backupFiles.sh
@@ -0,0 +1,23 @@
+# Title: Back up all files in the folder
+# Author: Ahmed M Khan
+# Date Created: 6/27/21
+# Date Modified: 6/27/21
+# Description : This program backs up all the files in a directory specified
+
+#!/bin/bash
+
+dir='/home/ahmed/scripts'
+
+tar cvf /tmp/backup.tar $dir
+gzip /tmp/backup.tar
+
+find /tmp/backup.tar.gz -type f -mtime -1 -print &> /dev/null
+
+if [ $? -eq 0 ]
+then
+        echo 'Backup was successful'
+        echo 'Copying the backup file'
+        cp /tmp/backup.tar.gz /tmp/backup
+else
+        echo 'Backup failed'
+fi
