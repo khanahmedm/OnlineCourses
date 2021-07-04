adab331aae36f5a92f7c4eac492aa0032bd5b6d7
khanahmedm <81051236+khanahmedm@users.noreply.github.com>
2021-07-03 22:38:51 -0500

    Create backupCmdSubs.sh

---
+++ LinuxShellScripting/backupCmdSubs.sh
@@ -0,0 +1,54 @@
+# Title: Backup files using bash scripting techniques - use of command substitution
+# Author: Ahmed M Khan
+# Date Created: 7/03/21
+# Date Modified: 7/03/21
+# Description : This program shows use of command substitution to print date in the log file
+
+#!/bin/bash
+
+# $1 in LOGFILE stores the argument passed on the command line for log file name
+LOGFILE="/home/$USER/$1"
+BACKUP_LOC="/usr/bin/su*"
+BACKUP_TARGET="/home/$USER/backup"
+
+# Backing up files - note the use of && and ||
+# && only runs if the previous command has exit status of zero; || runs if exit status has non-zero value
+# Redirecting error output (stderr) of mkdir command to /dev/null goes to null device
+
+# Function for creating a backup directory and setting local variable
+function createBackupDir {
+echo 'Creating a backup directory' && mkdir $BACKUP_TARGET 2> /dev/null || echo 'Directory already exists'
+
+# Using command substitution to print date in the log file at the beginning of each run
+echo "$(date +"%x %r %Z")" >> $LOGFILE
+
+TESTVAR1='Test variable 1'
+local TESTVAR2='Test variable 2'
+echo $TESTVAR1
+echo $TESTVAR2
+
+}
+
+# Function for tail command
+tail () {
+        command tail -n $1
+}
+
+createBackupDir
+
+# Copying files
+# Redirecting both stdout and stderr of cp command to log file
+echo 'Copying files' && cp $BACKUP_LOC $BACKUP_TARGET >> $LOGFILE 2>&1
+
+# Finding case-insesitive pattern in the log file and showing the last two lines
+grep -i denied $LOGFILE | tail 2
+
+echo 'Printing exit status'
+echo $?
+
+echo $TESTVAR1
+# local variable will print blank
+echo $TESTVAR2
+
+# Setting exit code
+exit 127
