897b2226d952db9540152b02c931f82cbdd0aac8
khanahmedm <81051236+khanahmedm@users.noreply.github.com>
2021-07-04 18:48:37 -0500

    Create backupTrapSignal.sh

---
+++ LinuxShellScripting/BashScripting-acloudguru/backupTrapSignal.sh
@@ -0,0 +1,60 @@
+# Title: Backup files using bash scripting techniques - use of trap signal
+# Author: Ahmed M Khan
+# Date Created: 7/04/21
+# Date Modified: 7/04/21
+# Description : This program shows use of trapping interrupt signal
+
+#!/bin/bash
+
+# $1 in LOGFILE stores the argument passed on the command line for log file name
+LOGFILE="/home/$USER/$1"
+BACKUP_LOC="/usr/bin"
+BACKUP_TARGET="/home/$USER/backup"
+
+# Backing up files - note the use of && and ||
+# && only runs if the previous command has exit status of zero; || runs if exit status has non-zero value
+# Redirecting error output (stderr) of mkdir command to /dev/null goes to null device
+
+# Function for creating a backup directory and setting local variable
+
+function createBackupDir {
+echo 'Creating a backup directory' && mkdir $BACKUP_TARGET 2> /dev/null || echo 'Directory already exists'
+
+# Using command substitution to print date in the log file at the beginning of each run
+echo "$(date +"%x %r %Z")" >> $LOGFILE
+
+}
+
+# Function for tail command
+tail () {
+        command tail -n $1
+}
+
+cleanup () {
+        rm -rf $BACKUP_TARGET
+        echo "RECEIVED CTRLC" >> $LOGFILE
+}
+
+createBackupDir
+
+trap cleanup SIGINT
+
+# Copying files
+# Redirecting both stdout and stderr of cp command to log file
+#echo 'Copying files' && cp -v $BACKUP_LOC $BACKUP_TARGET >> $LOGFILE 2>&1
+
+# Copying file using for loop
+cd $BACKUP_LOC
+for i in $( ls )
+do
+        cp -v $i $BACKUP_TARGET/$i-backup >> $LOGFILE 2>&1
+done
+
+# Finding case-insesitive pattern in the log file and showing the last two lines
+grep -i denied $LOGFILE | tail 2
+
+echo 'Printing exit status'
+echo $?
+
+# Setting exit code
+exit 127
